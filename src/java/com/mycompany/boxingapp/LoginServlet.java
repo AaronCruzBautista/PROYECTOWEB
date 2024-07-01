097dca3d049d1046c3dab3682841a1ffcef26241
AaronCruzBautista <aaron.cruz08@hotmail.com>
2024-07-01 00:06:44 -0600

    se sube proyecto

---
+++ src/java/com/mycompany/boxingapp/LoginServlet.java
@@ -0,0 +1,69 @@
+
+package com.mycompany.boxingapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+ @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        if ("admin".equals(username) && "1234".equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            
+            Cookie userCookie = new Cookie("username", username);
+            userCookie.setMaxAge(60*60*24); // 1 day
+            response.addCookie(userCookie);
+            
+            response.sendRedirect("home");
+        } else {
+            response.sendRedirect("login.html");
+        }
+    }   // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
